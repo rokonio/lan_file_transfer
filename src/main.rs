72a692a9bb97c9754bc841672f98c91c16c1d866
Rokonio <naelkanada+git@gmail.com>
2023-02-01 18:52:25 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:0")?;
+    let port = listener.local_addr()?.port();
+    dbg!(listener);
+    Ok(())
 }
